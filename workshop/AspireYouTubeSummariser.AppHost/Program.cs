e5c006e3759743e1e9993d6e1e122a29471e29cf
wlstmd <jinwoo8288@gmail.com>
2024-07-31 15:57:02 +0900

    cicd test

--- workshop/AspireYouTubeSummariser.AppHost/Program.cs
+++ workshop/AspireYouTubeSummariser.AppHost/Program.cs
@@ -5,6 +5,7 @@ var cache = builder.AddRedis("cache");
 var config = builder.Configuration;
 
 var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("apiapp")
+                    .WithExternalHttpEndpoints()
                     .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
                     .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
                     .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
